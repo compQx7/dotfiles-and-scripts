c547efc100a6eecd36bdddaedd2693e237497158
masa <ilfg1221@gmail.com>
2024-12-16 08:27:53 +0900

    modify telescope settings

--- TODO.md
+++ TODO.md
@@ -1,18 +1,24 @@
 # TODO
 
 - WSL2設定
+- log repository を分離
 
 - telescope の検索結果を再度見たい
+  - resume を利用した関数を作成し、デフォルトでキャッシュを利用するように変更
 
 - lazygit では`<Leader>to`に反応しないようにしたい
   - 通常のターミナルでのみにしたい
   - 現状`<C-t>`によるindentが効かない
+- toggle-term のトグルショートカット
+  - 現状`<C-t>`に割り当てているため、insertモードでindentが効かない
+  - `<Plug>(xxx)`名前空間で解決できる？
 
 - copilot のtab補完がうっとうしい？
 
 - js環境
   - import をコードアクションで補完したい
   - jestのテストを個別に実行したい
+    - neovim のテスト系プラグインでできそう
   - フロントエンド用の設定を on/off できるようにしたい
 - プロジェクト全体での置換
 - vscodeのようにコミット履歴を見やすくしたい
@@ -24,10 +30,6 @@
   - mfussenegger/nvim-lint
 
 - keymap に description を追加したい
-
-- auto-session
-  - 最近開いたプロジェクトを開けるとなおよい
-  - mark も復元できるとよい
 
 - fold の活用
 - quickfix の活用
--- vim/nvim/lua/plugins/search/telescope.lua
+++ vim/nvim/lua/plugins/search/telescope.lua
@@ -1,3 +1,26 @@
+verticalLayout1 = {
+	layout_strategy = "vertical",
+	layout_config = {
+		preview_height = 0.5,
+		vertical = {
+			mirror = false, -- Toggle window to top/bottom
+		},
+	},
+}
+verticalLayout2 = {
+	layout_strategy = "vertical",
+	layout_config = {
+		preview_height = 0.5,
+		vertical = {
+			mirror = false, -- Toggle window to top/bottom
+		},
+	},
+	path_display = function(opts, path)
+		local tail = require("telescope.utils").path_tail(path)
+		return string.format("%s (%s)", tail, path)
+	end,
+}
+
 local telescope = {
 	'nvim-telescope/telescope.nvim',
 	branch = '0.1.x',
@@ -7,37 +30,10 @@ local telescope = {
 	config = function()
 		require('telescope').setup({
 			pickers = {
-				find_files = {
-					layout_strategy = "vertical",
-					layout_config = {
-						preview_height = 0.5,
-						vertical = {
-							mirror = false, -- Toggle window to top/bottom
-						},
-					},
-					path_display = function(opts, path)
-						local tail = require("telescope.utils").path_tail(path)
-						return string.format("%s (%s)", tail, path)
-					end,
-				},
-				live_grep = {
-					layout_strategy = "vertical",
-					layout_config = {
-						preview_height = 0.5,
-						vertical = {
-							mirror = false, -- Toggle window to top/bottom
-						},
-					},
-				},
-				current_buffer_fuzzy_find = {
-					layout_strategy = "vertical",
-					layout_config = {
-						preview_height = 0.5,
-						vertical = {
-							mirror = false, -- Toggle window to top/bottom
-						},
-					},
-				},
+				find_files = verticalLayout2,
+				live_grep = verticalLayout1,
+				current_buffer_fuzzy_find = verticalLayout1,
+				buffers = verticalLayout2,
 			}
 		})
 		local builtin = require('telescope.builtin')
@@ -47,6 +43,7 @@ local telescope = {
 		vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope current_buffer_fuzzy_find<CR>')
 		vim.keymap.set('n', '<Leader>fgc', '<cmd>Telescope git_commits<CR>')
 		vim.keymap.set('n', '<Leader>fgs', '<cmd>Telescope git_status<CR>')
+		vim.keymap.set('n', '<Leader>fr', '<cmd>Telescope resume<CR>')
 
 		function _G.live_grep_in_directory()
 			local directory_path = vim.fn.input('Search path: ', '', 'file')
