b42f18bd42a6f516cfb7bf9965c7a4422d95d6d6
masa <ilfg1221@gmail.com>
2024-12-17 08:45:06 +0900

    modify telescope setting

--- vim/nvim/lua/plugins/search/telescope.lua
+++ vim/nvim/lua/plugins/search/telescope.lua
@@ -29,21 +29,72 @@ local telescope = {
 	},
 	config = function()
 		require('telescope').setup({
+			defaults = {
+				layout_strategy = "vertical",
+				cache_picker = {
+					num_pickers = 10,
+				},
+				file_ignore_patterns = {
+					'.git',
+					'.cache',
+					'node_modules',
+				},
+				vim_grep_arguments = {
+					'rg',
+					-- '--color=never',
+					-- '--no-heading',
+					'--with-filename',
+					'--line-number',
+					'--column',
+					'--smart-case',
+					'-uu',
+				},
+			},
 			pickers = {
 				find_files = verticalLayout2,
 				live_grep = verticalLayout1,
 				current_buffer_fuzzy_find = verticalLayout1,
 				buffers = verticalLayout2,
-			}
+			},
+			extensions = {
+				-- fzf = {
+				-- 	fuzzy = true,
+				-- 	override_generic_sorter = true,
+				-- 	override_file_sorter = true,
+				-- 	case_mode = "smart_case",
+				-- },
+			},
 		})
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<Leader>fn', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<Leader>fN', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<Leader>fa', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<Leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 		vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope current_buffer_fuzzy_find<CR>')
 		vim.keymap.set('n', '<Leader>fgc', '<cmd>Telescope git_commits<CR>')
 		vim.keymap.set('n', '<Leader>fgs', '<cmd>Telescope git_status<CR>')
 		vim.keymap.set('n', '<Leader>fr', '<cmd>Telescope resume<CR>')
+
+		local function myresume(picker_title)
+			local cached_pickers = require('telescope.state').get_global_key('cached_pickers')
+			if not cached_pickers then
+				return
+			end
+			for i, picker in pairs(cached_pickers) do
+				if picker.prompt_title == picker_title then
+					return i
+				end
+			end
+			return
+		end
+		function _G.find_files_or_resume()
+			local index = myresume('Find Files')
+			if index ~= nil then
+				builtin.resume({ cache_index = index })
+			else
+				builtin.find_files()
+			end
+		end
+		vim.keymap.set('n', '<Leader>fn', '<cmd>lua find_files_or_resume()<CR>', { noremap = true, silent = true })
 
 		function _G.live_grep_in_directory()
 			local directory_path = vim.fn.input('Search path: ', '', 'file')
