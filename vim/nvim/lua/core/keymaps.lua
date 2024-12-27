17bba74ba57543bc8a1d8f28dc2d9323d84c370a
masa <ilfg1221@gmail.com>
2024-12-28 07:21:44 +0900

    modify keymaps

--- vim/nvim/lua/core/keymaps.lua
+++ vim/nvim/lua/core/keymaps.lua
@@ -1,26 +1,28 @@
 vim.api.nvim_set_var('mapleader', ' ')
 
--- TODO: Window size
+vim.keymap.set({ 'n', 'x' }, 'J', '5j')
+vim.keymap.set({ 'n', 'x' }, 'K', '5k')
+vim.keymap.set({ 'n', 'x' }, '<Leader>h', '0')
+vim.keymap.set({ 'n', 'x' }, '<Leader>l', '$')
+vim.keymap.set({ 'n', 'x' }, '<Leader>m', '%')
 
-vim.keymap.set('n', 'J', '5j')
-vim.keymap.set('n', 'K', '5k')
-vim.keymap.set('n', '<Leader>h', '0')
-vim.keymap.set('n', '<Leader>l', '$')
-vim.keymap.set('n', '<Leader>m', '%')
-vim.keymap.set('n', '<C-p>', ':bp<CR>')
-vim.keymap.set('n', '<C-n>', ':bn<CR>')
-vim.keymap.set('n', '<Leader>/', ':noh<CR>')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<CR>')
+vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<CR>')
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<CR>')
 
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 vim.keymap.set('n', '*', '*zz')
 vim.keymap.set('n', '#', '#zz')
+vim.keymap.set('n', '<Leader>/', ':noh<CR>')
 
+vim.keymap.set('n', '<C-p>', ':bp<CR>')
+vim.keymap.set('n', '<C-n>', ':bn<CR>')
 vim.keymap.set('n', '<C-q>', '<cmd>bd<CR>')
-
-vim.keymap.set('v', 'J', '5j')
-vim.keymap.set('v', 'K', '5k')
-vim.keymap.set('v', '<Leader>h', '0')
-vim.keymap.set('v', '<Leader>l', '$')
-vim.keymap.set('v', '<Leader>m', '%')
 
